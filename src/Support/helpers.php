ef7075ada742cce311f0bfcc70f61d832ee92480
021-projects <devsellio@gmail.com>
2022-08-26 18:44:20 +0300

    fix(from_wei_to_eth): use unit helper instead of web3 utils class

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -1,19 +1,10 @@
 <?php
 
-use Web3\Utils;
+use O21\CryptoWallets\Units\Ethereum;
 
 if (! function_exists('from_wei_to_eth')) {
     function from_wei_to_eth($number): string
     {
-        [$bnq, $bnr] = Utils::fromWei($number, 'ether');
-
-        $remainder = substr(
-            // https://github.com/web3p/web3.php/issues/241#issuecomment-1010806449
-            str_pad($bnr->toString(), 18, '0', STR_PAD_LEFT),
-            0,
-            8
-        );
-
-        return $bnq.'.'.$remainder;
+        return Ethereum::Ether->fromWei($number);
     }
 }
